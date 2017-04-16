c1d0a9835eb145eed94740cc9c1fac25d3db8598
ShreyDixit <shreydixit93@gmail.com>
2017-04-16 20:05:45 +0530

    boilerplate setup

---
+++ _build/js/mainapp.js
@@ -0,0 +1,1 @@
+!function(){function a(a,o){return{loadData:loadData}}angular.module("boilerplate").factory("getDataFromAPI",a),a.$inject=["$http","LocalStorage"]}(),function(){function a(a,o){console.log("dsasadads")}angular.module("boilerplate").controller("MainController",a),a.$inject=["LocalStorage","QueryService"]}();
